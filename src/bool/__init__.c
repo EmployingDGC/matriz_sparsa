9c5a4b2b2ec5a4ca99156ee044e34280cdec66a0
EmployingDGC <daviguizani@gmail.com>
2021-04-06 18:34:45 -0300

    implementação
    biblioteca boolean

---
+++ src/bool/__init__.c
@@ -0,0 +1,9 @@
+#ifndef BOOL
+#define BOOL
+
+#define  true 1
+#define false 0
+
+typedef char Bool;
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,11 @@
+// #include <stdlib.h>
+#include <stdio.h>
+#include "./bool/__init__.c"
+#include "./matrizSparsa/__init__.c"
+
+int main(int argc, char const *argv[])
+{
+    
+    
+    return 0;
+}
